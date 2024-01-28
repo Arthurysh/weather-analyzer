af6af7893044acc5385e2d6988ca98b26d3cb754
AlexeyTrofimenko <56361146+AlexeyTrofimenko@users.noreply.github.com>
2024-01-28 20:33:21 +0200

    Add api test cases

---
+++ backend/core/web/tests/test_api.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from core.web.views.data import collect_data
+
+class TestCollectData(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.result = collect_data(52.52, 13.41, "2023-01-01")
+
+    def test_result_is_dict(self):
+        self.assertIsInstance(self.result, dict)
+
+    def test_result_has_time_key(self):
+        self.assertIn('time', self.result)
+
+    def test_result_has_temperature_key(self):
+        self.assertIn('temperature_2m_mean', self.result)
+
+    def test_result_has_precipitation_key(self):
+        self.assertIn('precipitation_sum', self.result)
+
+    def test_result_has_wind_speed_key(self):
+        self.assertIn('wind_speed_10m_max', self.result)
+    
+    def test_result_len_is_correct(self):
+        self.assertEqual(len(self.result.get("time")), 3288)
